d5b6e8639ba0e40724fd97f7e28a81a8fea97889
Stéphane BOURET <sbouret@orange.fr>
2024-02-07 14:26:50 +0100

    Création des Doctrine Listeners pour les slugs Category et Product

---
+++ src/Doctrine/Listener/ProductSlugListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Doctrine\Listener;
+
+use App\Entity\Product;
+use Doctrine\ORM\Events;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: Product::class)]
+class ProductSlugListener
+{
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Product $entity, PrePersistEventArgs $event)
+    {
+        // $entity = $event->getObject();
+
+        // if (!$entity instanceof Product) {
+        //     return;
+        // }
+
+        if (empty($entity->getSlug())) {
+            // SluggerInterface
+            $entity->setSlug(strtolower($this->slugger->slug($entity->getName())));
+        }
+    }
+}
